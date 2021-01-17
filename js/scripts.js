8eec5568fdca65b97a14c8c6c364a1bc85bbe02c
dagrimb <74441727+dagrimb@users.noreply.github.com>
2021-01-17 08:34:02 -0800

    add list item and attach value of input to it

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 
 function newItem() {
 
+//Add new item to list of items. Set li variable to li nodes. Set new variable = to the value of 
+// id="input" in the form element with a class of "toDoList". Append the value of the input to the list
 
-
-
-
+let li = $('<li></li>');
+let inputValue = $('#input').val();
+li.append(inputValue);
 
 
 
